ae6917d2dcfa0cf78832fb67f527181ae533e9b5
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-11-02 14:56:57 -0700

    Update and rename test to main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <string>
+#include "tests.h"
+
+void print_this(const std::vector<int>& nums);
+void print_backwards(const std::vector<int>& nums);
+
+//functions below have tests implemented for them
+
+
+
+int main() {
+
+	execute_tests();
+
+	std::cin.get();
+	return 0;
+}
+
+void print_this(const std::vector<int>& nums) {
+	// complete.. 
+}
+
+void print_backwards(const std::vector<int>& nums) {
+	// complete..
+}
+
+std::vector<int> reverse_this(const std::vector<int>& nums) {
+	// complete..
+}
+
+int find_max(const std::vector<int>& nums) {
+ // complete.. return -1 for errors
+}
+
+int find_min(const std::vector<int>& nums) {
+  // complete.. return -1 for errors
+}
+
